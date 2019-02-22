2367d4c87da001825bba5bad820bac71d3666af3
Apoorv Mittal <apoorv@umd.edu>
2019-02-22 12:17:01 -0500

    Added drupal backend in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+        baseUrl: `http://drupaltest.dd:8083`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
