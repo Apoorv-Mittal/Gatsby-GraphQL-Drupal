b47334ace2b6dc1fed339817460945574b5ca6bc
Apoorv Mittal <apoorv@umd.edu>
2019-02-22 21:19:37 -0500

    Made a Article page

---
+++ src/pages/article.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export class Article extends React.Component {
+  
+  render() {
+
+    let image = this.props.location.state.image[0].node.original.src
+    let node = this.props.location.state.node
+
+    console.log(image)
+    return (
+      <Layout>
+        <SEO title={node.title} />
+        <h1>{node.title}</h1>
+        <img src={image} alt ="this is a recipe" style={{width: "100%"}}/>
+        <article dangerouslySetInnerHTML={{ __html: node.body.value }} />
+        <br/>
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    )
+  }
+}
+
+export default Article
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 
 class IndexPage extends React.Component {
 
   render() {
+
     const pages = this.props.data.allNodeArticle.edges
-    const pageTitles = pages.map(page => <li>{page.node.title}</li>)
+    const pageTitles = pages.map((page, index) => 
+    {
+      let node = page.node
+      let img = this.props.data.allImageSharp.edges.filter(i => i.node.parent.id === node.relationships.field_image.localFile.id )
+      return (
+        <li key ={Math.random()}>
+          <Link to="/article" key ={Math.random()} state={{ node: node, image: img }}>
+            {page.node.title}
+          </Link>
+        </li>)
+    })
     return (
       <Layout>
         <ul>{pageTitles}</ul>
@@ -22,6 +33,38 @@ export const query = graphql`
       edges {
         node {
           title
+          body {
+            value
+            processed
+          }
+          relationships {
+            field_image {
+              id,
+              localFile {
+                id
+              }
+            }
+          }
+        }
+      }
+    }
+    allImageSharp {
+      edges {
+        node {
+          original {
+            width
+            height
+            src
+          },
+          id,
+          internal {
+            contentDigest
+            type
+            owner
+          },
+          parent {
+            id
+          }
         }
       }
     }
