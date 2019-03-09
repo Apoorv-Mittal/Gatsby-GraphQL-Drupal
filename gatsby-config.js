a859f2e26c1d561a5e524272275c2a9f0c3a5eb2
Apoorv Mittal <apoorv@umd.edu>
2019-03-08 19:28:20 -0500

    Added Gatsby plugin for SASS

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
         baseUrl: `http://drupaltest.dd:8083`,
       },
     },
+    `gatsby-plugin-sass`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
