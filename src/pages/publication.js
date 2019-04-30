3611c80374ad977dd0dbcf2cc05650421b3b2503
Apoorv Mittal <apoorv@umd.edu>
2019-04-30 07:52:04 -0400

    Added publication route

---
+++ src/pages/publication.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+export default class Publication extends React.Component {
+  render() {
+    let data = this.props.data.allNodePublication.edges;
+
+    console.log(data);
+
+    return (
+      <Layout>
+        <SEO title="Publications" />
+        {data.map((pub, index) => (
+          <div key={index}>
+            <h5>{pub.node.title}</h5>
+            <p>{pub.node.body.value}</p>
+          </div>
+        ))}
+        <br />
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    );
+  }
+}
+
+export const query = graphql`
+  query publicationQuery {
+    allNodePublication {
+      edges {
+        node {
+          title
+          body {
+            value
+          }
+        }
+      }
+    }
+  }
+`;
